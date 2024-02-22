9896929ab9d03c7c6a6cde57a06f4ee86a6b8c36
dhanush2bot <149936819+dhanush2bot@users.noreply.github.com>
2024-02-22 22:49:25 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -4,6 +4,7 @@ import string
 import random
 from configs import Config
 from pyrogram import Client
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
@@ -33,8 +34,10 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
         await message.delete()
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
+    button = InlineKeyboardMarkup([[InlineKeyboardButton("Click Here", url="http://example.com")]])
     sent_message = await media_forward(bot, user_id, file_id)
     await reply_forward(message=sent_message, file_id=file_id)
+    await bot.send_message(user_id, "Here is your file:", reply_markup=button)
     asyncio.create_task(delete_after_delay(sent_message, 1800))
 
 async def delete_after_delay(message, delay):
