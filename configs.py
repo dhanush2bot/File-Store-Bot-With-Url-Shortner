aa7a2027a0b5bb043ab6301c37fecd69e0f6f0cf
dhanush2bot <149936819+dhanush2bot@users.noreply.github.com>
2024-02-22 22:54:49 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -16,7 +16,7 @@ class Config(object):
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
-  OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", False))
+  OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 
 
   HOME_TEXT = f"""
